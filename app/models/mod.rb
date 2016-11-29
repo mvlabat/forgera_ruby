22848cfe81872f95624574763f979c2f351a2cfc
mvlabat <mvlabat@gmail.com>
2016-11-29 21:33:16 +0200

    Declare CurseScrapper and provide unit tests for it

--- app/models/mod.rb
+++ app/models/mod.rb
@@ -1,3 +1,7 @@
 class Mod < ApplicationRecord
-  
+  MINECRAFT_VERSION = '1.7.10'
+
+  def self.minecraft_version
+    MINECRAFT_VERSION
+  end
 end
---
+++ app/services/mod_scrapper/curse_scrapper.rb
@@ -0,0 +1,5 @@
+class CurseScrapper
+  def scrap(url)
+    mod = Mod.new(project_url: url)
+  end
+end
